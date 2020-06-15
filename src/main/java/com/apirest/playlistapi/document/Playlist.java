fc80cbccac0c9c128329259626915f8ed7d79faf
Alessandro Tulioli <desmecad@hotmail.com>
2020-06-15 20:10:02 -0300

    Update Playlist.java

--- src/main/java/com/apirest/playlistapi/document/Playlist.java
+++ src/main/java/com/apirest/playlistapi/document/Playlist.java
@@ -1,31 +1,65 @@
-package com.apirest.playlistapi.document;
+package com.spring.agendalive.document;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import java.time.LocalDateTime;
 
 @Document
-public class Playlist {
-	
-	@Id
-	private String id;
-	private String nome;
-		
-	public Playlist(String id, String nome) {
-		super();
-		this.id = id;
-		this.nome = nome;
-	}
-	
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+public class LiveDocument {
+
+    @Id
+    private String id;
+    private String liveName;
+    private String channelName;
+    private LocalDateTime liveDate;
+    private String liveLink;
+    private LocalDateTime registrationDate;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getLiveName() {
+        return liveName;
+    }
+
+    public void setLiveName(String liveName) {
+        this.liveName = liveName;
+    }
+
+    public String getChannelName() {
+        return channelName;
+    }
+
+    public void setChannelName(String channelName) {
+        this.channelName = channelName;
+    }
+
+    public LocalDateTime getLiveDate() {
+        return liveDate;
+    }
+
+    public void setLiveDate(LocalDateTime liveDate) {
+        this.liveDate = liveDate;
+    }
+
+    public String getLiveLink() {
+        return liveLink;
+    }
+
+    public void setLiveLink(String liveLink) {
+        this.liveLink = liveLink;
+    }
+
+    public LocalDateTime getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(LocalDateTime registrationDate) {
+        this.registrationDate = registrationDate;
+    }
 }
